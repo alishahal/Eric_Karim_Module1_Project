f54b2956a5b4e21078e6a6a6be7cc164c74f0916
Eric Johnson <johnson.n.eric@gmail.com>
2020-05-05 15:52:58 -0500

    some functionality

---
+++ lib/purchase_item.rb
@@ -0,0 +1,18 @@
+require_relative './p_readyforcheckout.rb'
+require 'pry'
+
+def create_order(order_item)
+
+end
+
+def find_user_bill(name)
+
+end
+
+def bill_orders(bill)
+
+end
+
+def buy_order_or_view_bill
+
+end
