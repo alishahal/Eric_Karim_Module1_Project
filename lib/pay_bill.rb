30eab4b54defc88ea2659dc4f023587ef15fe282
Eric Johnson <johnson.n.eric@gmail.com>
2020-05-07 10:52:10 -0500

    All functionalities are WORKING. Now I can decide what additional functions I might want to add.

--- lib/pay_bill.rb
+++ lib/pay_bill.rb
@@ -1,22 +1,15 @@
 require 'pry'
 
 def pay
-    ## room here for past items
-    ## room here for redeeming old stuff
-
-    current_user = User.find_by(name: $name_input)
-    users_bill = Bill.find_by(user_id: current_user.id)
-
-    bill_total = 0
-    bill_items = []
-
-    users_bill.orders.each do |order|
-        bill_items << order.item.item_name
-        bill_total += order.item.price
-    end
-
+    users_bill = find_user_bill($name_input)
+    bill_order_items = bill_items(users_bill)  
+    
+    total = 0
+    bill_order_items.each{|order|total+=order.price}
+    puts total.to_s
+    
     users_bill.orders.clear
-    puts "placeholder thank you"
+    puts "Thank you so much! Have a dope day!"
 end
 
 def ready_to_pay?
